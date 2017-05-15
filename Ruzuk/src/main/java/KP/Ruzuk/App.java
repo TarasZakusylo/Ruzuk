ca7c468532ef7c5cc923a1c4d9d732bcb4455c57
Zakk <tapac.zakusylo@gmail.com>
2017-05-15 21:57:46 +0300

    Start

---
+++ Ruzuk/src/main/java/KP/Ruzuk/App.java
@@ -0,0 +1,13 @@
+package KP.Ruzuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
